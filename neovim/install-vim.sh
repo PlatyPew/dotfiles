4daad646752a849d5168b8efb1073734dd3600c8
PlatyPew <daryl.limkz@gmail.com>
2019-02-03 10:38:11 +0800

    At times like this I wish I was a Garbage Man.

--- neovim/install-vim.sh
+++ neovim/install-vim.sh
@@ -8,32 +8,8 @@ mkdir -p ~/.config/nvim
 
 mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.bak 2> /dev/null
 
-cat << EOF > ~/.config/nvim/init.vim
-call plug#begin()
-Plug 'liuchengxu/space-vim-dark'
-Plug 'vim-airline/vim-airline'
-Plug 'vim-airline/vim-airline-themes'
-Plug 'ryanoasis/vim-devicons'
-Plug 'junegunn/limelight.vim'
-Plug 'junegunn/vim-journal'
-Plug 'junegunn/rainbow_parentheses.vim'
-Plug 'airblade/vim-gitgutter'
-Plug 'scrooloose/nerdtree'
-Plug 'shougo/deoplete.nvim', { 'do': ':UpdateRemotePlugins' }
-Plug 'terryma/vim-multiple-cursors'
-Plug 'junegunn/goyo.vim'
-Plug 'tpope/vim-fugitive'
-Plug 'vim-scripts/LargeFile'
-Plug 'junegunn/fzf', { 'dir': '~/.fzf', 'do': './install --bin' }
-Plug 'junegunn/fzf.vim'
-Plug 'mbbill/undotree', { 'on': 'UndotreeToggle' }
-Plug 'terryma/vim-multiple-cursors'
-Plug 'w0rp/ale'
-call plug#end() 
-EOF
-
 python3 -m pip install neovim
-nvim -c ':PlugInstall' -c ':UpdateRemotePlugins' -c ':qall' # Installs neovim plugins
 
-rm ~/.config/nvim/init.vim
+nvim -u <(sed '/call plug#end/q' ${dir}init.vim) ':PlugInstall' -c ':qall'
+
 ln -s ${dir}init.vim ~/.config/nvim/init.vim
