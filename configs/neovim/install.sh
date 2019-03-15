4a7bd186503285e6c41d6dce68475fc2e12ccc8d
PlatyPew <daryl.limkz@gmail.com>
2019-03-15 22:11:11 +0800

    Added installation script for neovim

---
+++ configs/neovim/install.sh
@@ -0,0 +1,10 @@
+dir=$(echo "$(cd "$(dirname "$1")"; pwd)/$(basename "$1")") # Finds the current absolute path of file
+
+# Installs packages
+brew install npm python3 ctags-exuberant neovim
+python3 -m pip install neovim flake8
+npm install -g tern livedown eslint eslint-config-google esctags
+
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim # Installs Vim-plug
+
+nvim -u <(sed '/call plug#end/q' ${dir}init.vim) ':PlugInstall' -c ':qall'
