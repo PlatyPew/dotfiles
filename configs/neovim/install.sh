7381582425074ae345ef0159bc9bb42ffb2352a0
PlatyPew <daryl.limkz@gmail.com>
2019-03-16 13:04:40 +0800

    Updated neovim installation script

--- configs/neovim/install.sh
+++ configs/neovim/install.sh
@@ -5,4 +5,6 @@ npm install -g tern livedown eslint eslint-config-google esctags
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim # Installs Vim-plug
 
+dir=$(echo "$(cd "$(dirname "$1")"; pwd)/$(basename "$1")") # Finds the current absolute path of file
+cd ${dir}
 nvim -u <(sed '/call plug#end/q' init.vim) '+PlugInstall | qall'
