dab4367285ebdf8c0720560b20b8542f5b6acfea
PlatyPew <daryl.limkz@gmail.com>
2019-03-16 14:41:21 +0800

    Fixed neovim configurations

--- configs/neovim/install.sh
+++ configs/neovim/install.sh
@@ -6,4 +6,6 @@ npm install -g tern livedown eslint eslint-config-google esctags
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim # Installs Vim-plug
 
 BASE_PATH="configs/neovim"
-nvim -u <(sed '/call plug#end/q' $BASE_PATH/init.vim) -c ':PlugInstall' -c ':qall'
+sed '/call plug#end/q' $BASE_PATH/init.vim > $BASE_PATH/tmp.vim
+nvim -u $BASE_PATH/tmp.vim -c ':PlugInstall' -c ':qall'
+rm $BASE_PATH/tmp.vim
