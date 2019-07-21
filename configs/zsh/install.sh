804c8e08f806369d8369bd48f3ac7aa8304d4c71
Platy <daryl.limkz@gmail.com>
2019-07-21 20:10:15 +0800

    Updated zsh install script to include new plugins

--- configs/zsh/install.sh
+++ configs/zsh/install.sh
@@ -10,6 +10,8 @@ git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/b4b4r07/zsh-vimode-visual.git ~/.oh-my-zsh/custom/plugins/zsh-vimode-visual
+git clone https://github.com/DarrinTisdale/zsh-aliases-exa.git ~/.oh-my-zsh/custom/plugins/zsh-aliases-exa
+git clone https://github.com/hlissner/zsh-autopair.git ~/.oh-my-zsh/custom/plugins/zsh-autopair
 
 # Install zsh themes
 git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
