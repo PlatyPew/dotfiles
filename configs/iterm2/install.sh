8a22db3341c36efbe12cdcad137687a1f1f59d31
Platy <daryl.limkz@gmail.com>
2019-12-12 08:03:14 +0800

    Updated installation of brew formulae and cask to work

--- configs/iterm2/install.sh
+++ configs/iterm2/install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 # Install packages
+brew tap homebrew/cask-fonts
 brew cask install iterm2 homebrew/cask-fonts/font-inconsolata-nerd-font
+brew untap homebrew/cask-fonts
--- configs/macos/install.sh
+++ configs/macos/install.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # Install packages
+brew tap mongodb/brew
 brew install $(cat packagelist/brewformulae.txt)
+brew untap mongodb/brew
 brew tap homebrew/cask-fonts
 brew cask install --force $(cat packagelist/brewcask.txt)
 brew untap homebrew/cask-fonts
