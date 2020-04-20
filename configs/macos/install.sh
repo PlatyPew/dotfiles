cffbe545bf6348ba4ddbc09390cceed6ae03024c
Platy <daryl.limkz@gmail.com>
2020-04-20 08:29:26 +0800

    Modify configs/macos/install.sh, packagelist/brewcask.txt

--- configs/macos/install.sh
+++ configs/macos/install.sh
@@ -5,7 +5,9 @@ brew tap mongodb/brew
 brew install $(cat packagelist/brewformulae.txt)
 brew untap mongodb/brew
 brew tap homebrew/cask-fonts
+brew tap homebrew/cask-versions
 brew cask install --force $(cat packagelist/brewcask.txt)
 brew untap homebrew/cask-fonts
+brew untap homebrew/cask-versions
 npm -g install $(cat packagelist/npm.txt)
 pip3 install $(cat packagelist/pip.txt)
