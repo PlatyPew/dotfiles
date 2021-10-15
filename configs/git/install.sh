ba517eafb97aabe728a329da3e50f62869179a34
Platy <daryl.limkz@gmail.com>
2021-10-15 16:22:26 +0800

    fix(gpg-signing): use pinentry-mac instead of tui

--- configs/git/install.sh
+++ configs/git/install.sh
@@ -4,3 +4,4 @@
 git submodule init configs/git/gitz
 git submodule update --remote --recursive
 brew install git-lfs gnupg pinentry-mac commitizen
+echo "pinentry-program /usr/local/bin/pinentry-mac" >> ~/.gnupg/gpg-agent.conf
