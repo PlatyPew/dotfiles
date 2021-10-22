09d49114a9611955bbf20809b5e9177e152e683f
Platy <daryl.limkz@gmail.com>
2021-10-22 23:06:14 +0800

    fix(neovim): manually install tabnine for AS macs

--- configs/neovim/install.sh
+++ configs/neovim/install.sh
@@ -8,3 +8,18 @@ BASE_PATH="configs/neovim"
 sed '/call plug#end/q' $BASE_PATH/init.vim > $BASE_PATH/tmp.vim
 nvim -u $BASE_PATH/tmp.vim --headless +PlugInstall +qall
 rm $BASE_PATH/tmp.vim
+
+if [[ "$(uname -m)" == "arm64" ]]; then
+    COQ_PATH="$HOME/.config/nvim/plugged/coq_nvim/.vars"
+    TABNINE_VERSION=$(curl -fsSL https://update.tabnine.com/bundles/version)
+
+    mkdir -p $COQ_PATH/$TABNINE_VERSION/t9
+    mkdir -p $COQ_PATH/clients/t9
+
+    curl -sS https://update.tabnine.com/bundles/$TABNINE_VERSION/aarch64-apple-darwin/TabNine.zip > $BASE_PATH/TabNine.zip
+    unzip -d $COQ_PATH/$TABNINE_VERSION/t9 $BASE_PATH/TabNine.zip
+    chmod +x $COQ_PATH/$TABNINE_VERSION/t9/*
+    cp $COQ_PATH/$TABNINE_VERSION/t9/TabNine $COQ_PATH/clients/t9/
+
+    rm $BASE_PATH/TabNine.zip
+fi
