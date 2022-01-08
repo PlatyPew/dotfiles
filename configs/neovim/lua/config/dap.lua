c8abfb2d1b6e9deacd751c8a09bb0b0ad6cf673e
Platy <daryl.limkz@gmail.com>
2022-01-09 00:15:17 +0800

    feat(neovim): working on java dap
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- configs/neovim/lua/config/dap.lua
+++ configs/neovim/lua/config/dap.lua
@@ -26,6 +26,14 @@ dap.configurations.cpp = {
 dap.configurations.c = dap.configurations.cpp
 dap.configurations.rust = dap.configurations.cpp
 
+dap.adapters.java = function(callback, config)
+    M.execute_command({ command = "vscode.java.startDebugSession" }, function(err0, port)
+        assert(not err0, vim.inspect(err0))
+
+        callback({ type = "server", host = "127.0.0.1", port = port })
+    end)
+end
+
 require("nvim-dap-virtual-text").setup()
 
 require("dapui").setup()
--- configs/neovim/lua/config/lsp.lua
+++ configs/neovim/lua/config/lsp.lua
@@ -50,6 +50,35 @@ lspinstall.on_server_ready(function(server)
         }
     elseif server.name == "html" or server.name == "emmet_ls" then
         config.filetypes = { "html", "css", "javascriptreact" }
+    elseif server.name == "jdtls" then
+        local path = vim.fn.stdpath("data")
+
+        --[[ local cmd = {
+            "java",
+            "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+            "-Dosgi.bundles.defaultStartLevel=4",
+            "-Declipse.product=org.eclipse.jdt.ls.core.product",
+            "-Dlog.protocol=true",
+            "-Dlog.level=ALL",
+            "-Xms1g", "-Xmx2G",
+            "-javaagent:" .. path .. "/lsp_servers/jdtls/lombok.jar",
+            "--add-modules=ALL-SYSTEM",
+            "--add-opens", "java.base/java.util=ALL-UNNAMED",
+            "--add-opens", "java.base/java.lang=ALL-UNNAMED",
+            "-jar", path .. "/lsp_servers/jdtls/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar",
+            "-configuration", path .. "/lsp_servers/jdtls/config_mac",
+            "-data", vim.fn.getenv("HOME") .. "/workspace/" .. vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t'),
+        } ]]
+
+        local init_options = {
+            bundles = {
+                vim.fn.glob(
+                    path
+                        .. "/dapinstall/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar"
+                ),
+            },
+        }
+        config.init_options = init_options
     end
 
     server:setup(coq.lsp_ensure_capabilities(config))
