447ce33942b4daa0c34b79f0bffb26d07556a619
Platy <daryl.limkz@gmail.com>
2022-01-09 10:09:18 +0800

    fix(neovim): attach dap to lsp
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- configs/neovim/lua/config/lsp.lua
+++ configs/neovim/lua/config/lsp.lua
@@ -20,7 +20,6 @@ vim.g.coq_settings = {
     },
 }
 
-local lspconfig = require("lspconfig")
 local coq = require("coq")
 local coq_3p = require("coq_3p")
 local lspinstall = require("nvim-lsp-installer")
@@ -35,6 +34,11 @@ lspinstall.on_server_ready(function(server)
         on_attach = function(client)
             client.resolved_capabilities.document_formatting = false
             client.resolved_capabilities.document_range_formatting = false
+
+            if client.name == "jdtls" then
+                require("jdtls").setup_dap({ hotcodereplace = "auto" })
+                require("jdtls.dap").setup_dap_main_class_configs()
+            end
         end,
     }
 
@@ -51,34 +55,14 @@ lspinstall.on_server_ready(function(server)
     elseif server.name == "html" or server.name == "emmet_ls" then
         config.filetypes = { "html", "css", "javascriptreact" }
     elseif server.name == "jdtls" then
-        local path = vim.fn.stdpath("data")
-
-        --[[ local cmd = {
-            "java",
-            "-Declipse.application=org.eclipse.jdt.ls.core.id1",
-            "-Dosgi.bundles.defaultStartLevel=4",
-            "-Declipse.product=org.eclipse.jdt.ls.core.product",
-            "-Dlog.protocol=true",
-            "-Dlog.level=ALL",
-            "-Xms1g", "-Xmx2G",
-            "-javaagent:" .. path .. "/lsp_servers/jdtls/lombok.jar",
-            "--add-modules=ALL-SYSTEM",
-            "--add-opens", "java.base/java.util=ALL-UNNAMED",
-            "--add-opens", "java.base/java.lang=ALL-UNNAMED",
-            "-jar", path .. "/lsp_servers/jdtls/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar",
-            "-configuration", path .. "/lsp_servers/jdtls/config_mac",
-            "-data", vim.fn.getenv("HOME") .. "/workspace/" .. vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t'),
-        } ]]
-
-        local init_options = {
+        config.init_options = {
             bundles = {
                 vim.fn.glob(
-                    path
+                    vim.fn.stdpath("data")
                         .. "/dapinstall/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar"
                 ),
             },
         }
-        config.init_options = init_options
     end
 
     server:setup(coq.lsp_ensure_capabilities(config))
