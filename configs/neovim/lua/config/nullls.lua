dbc2e6d9299374067b0e25c569a1bc188b869157
Platy <daryl.limkz@gmail.com>
2022-01-14 21:52:05 +0800

    refactor(neovim): better code for nullls
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- configs/neovim/lua/config/nullls.lua
+++ configs/neovim/lua/config/nullls.lua
@@ -5,22 +5,39 @@ vim.cmd([[
 local null_ls = require("null-ls")
 local formatter_install = require("format-installer")
 
+local custom_configs = {
+    clang_format = {
+        extra_args = {
+            "--style",
+            "{IndentWidth: 4, PointerAlignment: Left, ColumnLimit: 100, AllowShortFunctionsOnASingleLine: Empty}",
+        },
+    },
+    prettier = {
+        extra_args = {
+            "--tab-width=4",
+            "--print-width=100",
+        },
+    },
+    stylua = {
+        extra_args = {
+            "--column-width=100",
+            "--indent-type=Spaces",
+        },
+    },
+    yapf = {
+        extra_args = {
+            "--style",
+            "{column_limit:100}",
+        },
+    },
+}
+
 local sources = {}
 for _, formatter in ipairs(formatter_install.get_installed_formatters()) do
     local config = { command = formatter.cmd }
-    if formatter.name == "clang_format" then
-        config["extra_args"] = {
-            "--style",
-            "{IndentWidth: 4, PointerAlignment: Left, ColumnLimit: 100, AllowShortFunctionsOnASingleLine: Empty}",
-        }
-    elseif formatter.name == "prettier" then
-        config["extra_args"] = { "--tab-width=4", "--print-width=100" }
-    elseif formatter.name == "stylua" then
-        config["extra_args"] = { "--column-width=100", "--indent-type=Spaces" }
-    elseif formatter.name == "yapf" then
-        config["extra_args"] = { "--style", "{column_limit:100}" }
+    if custom_configs[formatter.name] ~= nil then
+        config.extra_args = custom_configs[formatter.name].extra_args
     end
-
     table.insert(sources, null_ls.builtins.formatting[formatter.name].with(config))
 end
 
