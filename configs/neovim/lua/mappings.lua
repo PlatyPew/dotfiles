00b67d79095fd839a9b2b93e6bd86f7250b5573e
Platy <daryl.limkz@gmail.com>
2022-04-25 09:34:30 +0800

    refactor(neovim): refactored more mappings
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- configs/neovim/lua/mappings.lua
+++ configs/neovim/lua/mappings.lua
@@ -3,167 +3,175 @@ local remap = vim.keymap.set
 -- Vanilla
 -- Rebinds arrow keys to increase/decrease size of pane while in normal/visual mode
 -- Increase horizontal split
-remap("n", "<Up>", [[<Cmd>resize +2<CR>]], { noremap = true, silent = true })
-remap("v", "<Up>", [[<Cmd>resize +2<CR>]], { noremap = true, silent = true })
+remap("n", "<Up>", "<Cmd>resize +2<CR>")
+remap("v", "<Up>", "<Cmd>resize +2<CR>")
 
 -- Decrease horizontal split
-remap("n", "<Down>", [[<Cmd>resize -2<CR>]], { noremap = true, silent = true })
-remap("v", "<Down>", [[<Cmd>resize -2<CR>]], { noremap = true, silent = true })
+remap("n", "<Down>", "<Cmd>resize -2<CR>")
+remap("v", "<Down>", "<Cmd>resize -2<CR>")
 
 -- Decrease vertical split
-remap("n", "<Left>", [[<Cmd>vertical resize -2<CR>]], { noremap = true, silent = true })
-remap("v", "<Left>", [[<Cmd>vertical resize -2<CR>]], { noremap = true, silent = true })
+remap("n", "<Left>", "<Cmd>vertical resize -2<CR>")
+remap("v", "<Left>", "<Cmd>vertical resize -2<CR>")
 
 -- Increase vertical split
-remap("n", "<Right>", [[<Cmd>vertical resize +2<CR>]], { noremap = true, silent = true })
-remap("v", "<Right>", [[<Cmd>vertical resize +2<CR>]], { noremap = true, silent = true })
+remap("n", "<Right>", "<Cmd>vertical resize +2<CR>")
+remap("v", "<Right>", "<Cmd>vertical resize +2<CR>")
 
 -- Better window switching
 -- Move to pane on the left      Ctrl-h
-remap("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
+remap("n", "<C-h>", "<C-w>h")
 -- Move to lower pane            Ctrl-j
-remap("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
+remap("n", "<C-j>", "<C-w>j")
 -- Move to upper pane            Ctrl-j
-remap("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
+remap("n", "<C-k>", "<C-w>k")
 -- Move to pane on the right     Ctrl-h
-remap("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
+remap("n", "<C-l>", "<C-w>l")
 
 -- Remap semicolon to colon
-remap("n", ";", ":", { noremap = true })
+remap("n", ";", ":")
 
 -- Lazy colon, {noremap = true
-remap("n", ";", ":", { noremap = true })
+remap("n", ";", ":")
 
 -- Cycling buffers
 remap(
     "n",
     "<Leader>bh",
-    [[<Cmd>if &modifiable && !&readonly && &modified <CR> :write<CR> :endif<CR>:bfirst<CR><CR>]],
-    { noremap = true }
+    "<Cmd>if &modifiable && !&readonly && &modified <CR> :write<CR> :endif<CR>:bfirst<CR><CR>"
 )
 remap(
     "n",
     "<Leader>bj",
-    [[<Cmd>if &modifiable && !&readonly && &modified <CR> :write<CR> :endif<CR>:bnext<CR><CR>]],
-    { noremap = true }
+    "<Cmd>if &modifiable && !&readonly && &modified <CR> :write<CR> :endif<CR>:bnext<CR><CR>"
 )
 remap(
     "n",
     "<Leader>bk",
-    [[<Cmd>if &modifiable && !&readonly && &modified <CR> :write<CR> :endif<CR>:bprevious<CR><CR>]],
-    { noremap = true }
+    "<Cmd>if &modifiable && !&readonly && &modified <CR> :write<CR> :endif<CR>:bprevious<CR><CR>"
 )
 remap(
     "n",
     "<Leader>bl",
-    [[<Cmd>if &modifiable && !&readonly && &modified <CR> :write<CR> :endif<CR>:blast<CR><CR>]],
-    { noremap = true }
+    "<Cmd>if &modifiable && !&readonly && &modified <CR> :write<CR> :endif<CR>:blast<CR><CR>"
 )
-remap("n", "<Leader>bq", [[<Cmd>bdelete<CR>]], { noremap = true })
+remap("n", "<Leader>bq", "<Cmd>bdelete<CR>")
 
 -- Stops cursor from flying everywhere
-remap("n", "n", "nzzzv", { noremap = true })
-remap("n", "N", "Nzzzv", { noremap = true })
+remap("n", "n", "nzzzv")
+remap("n", "N", "Nzzzv")
 
 -- Better undo breakpoints
-remap("i", ",", ",<C-g>u", { noremap = true })
-remap("i", ".", ".<C-g>u", { noremap = true })
+remap("i", ",", ",<C-g>u")
+remap("i", ".", ".<C-g>u")
 
 -- Move stuff in visual mode
-remap("v", "J", [[:m '>+1'<CR>gv=gv]], { noremap = true })
-remap("v", "K", [[:m '<-2'<CR>gv=gv]], { noremap = true })
+remap("v", "J", [[:m '>+1'<CR>gv=gv]])
+remap("v", "K", [[:m '<-2'<CR>gv=gv]])
 
 -- Colorizer
-remap("n", "<Leader>T", [[<Cmd>Transparency<CR>]], { noremap = true, silent = true })
+remap("n", "<Leader>T", [[<Cmd>Transparency<CR>]])
 
 -- FZF-Lua
 remap("n", "<C-p>", function()
     require("fzf-lua").files({ cmd = 'rg --files --hidden --no-ignore-vcs -g "!.git/*"' })
-end, { noremap = true, silent = true })
+end)
 
 remap("n", "<C-g>", function()
     require("fzf-lua").live_grep_native()
-end, { noremap = true, silent = true })
+end)
 
 -- LSPSaga
 remap("n", "[g", function()
     require("lspsaga.diagnostic").lsp_jump_diagnostic_prev()
-end, { noremap = true, silent = true })
+end)
 remap("n", "]g", function()
     require("lspsaga.diagnostic").lsp_jump_diagnostic_next()
-end, { noremap = true, silent = true })
+end)
 remap("n", "ga", function()
     require("lspsaga.codeaction").code_action()
-end, { noremap = true, silent = true })
+end)
 remap("n", "gd", function()
     require("lspsaga.provider").preview_definition()
-end, { noremap = true, silent = true })
+end)
 remap("n", "gf", function()
     require("lspsaga.provider").lsp_finder()
-end, { noremap = true, silent = true })
+end)
 remap("n", "gh", function()
     require("lspsaga.hover").render_hover_doc()
-end, { noremap = true, silent = true })
+end)
 remap("n", "gr", function()
     require("lspsaga.rename").rename()
-end, { noremap = true, silent = true })
+end)
 
 -- COQ Autopairs
-remap("i", "<esc>", [[pumvisible() ? "<c-e><esc>" : "<esc>"]], { expr = true, noremap = true })
-remap("i", "<c-c>", [[pumvisible() ? "<c-e><c-c>" : "<c-c>"]], { expr = true, noremap = true })
-remap("i", "<tab>", [[pumvisible() ? "<c-n>" : "<tab>"]], { expr = true, noremap = true })
-remap("i", "<s-tab>", [[pumvisible() ? "<c-p>" : "<bs>"]], { expr = true, noremap = true })
+remap("i", "<esc>", [[pumvisible() ? "<c-e><esc>" : "<esc>"]])
+remap("i", "<c-c>", [[pumvisible() ? "<c-e><c-c>" : "<c-c>"]])
+remap("i", "<tab>", [[pumvisible() ? "<c-n>" : "<tab>"]])
+remap("i", "<s-tab>", [[pumvisible() ? "<c-p>" : "<bs>"]])
 
 -- UndoTree
-remap("n", "<Tab>", [[<Cmd>UndotreeToggle<CR>]], {})
+remap("n", "<Tab>", "<Cmd>UndotreeToggle<CR>")
 
 -- Format
 remap("n", "g=", function()
     vim.lsp.buf.formatting_sync()
-end, { noremap = true, silent = true })
+end)
 
 -- Instant
-remap("n", "<Leader>Is", [[<Cmd>call v:lua.InstantStartSession()<CR>]], {})
-remap("n", "<Leader>Ij", [[<Cmd>call v:lua.InstantJoinSession()<CR>]], {})
-remap("n", "<Leader>Iq", [[<Cmd>call v:lua.InstantStopSession()<CR>]], {})
-remap("n", "<Leader>IQ", [[<Cmd>call v:lua.InstantStopServer()<CR>]], {})
+remap("n", "<Leader>Is", function()
+    _G.InstantStartSession()
+end)
+remap("n", "<Leader>Ij", function()
+    _G.InstantJoinSession()
+end)
+remap("n", "<Leader>Iq", function()
+    _G.InstantStopSession()
+end)
+remap("n", "<Leader>IQ", function()
+    _G.InstantStopServer()
+end)
 
 -- Gitsigns
 remap("n", "<Leader>hd", function()
     require("gitsigns").diffthis()
-end, { noremap = true, silent = true })
-remap("n", "]c", "&diff ? ']c' : '<Cmd>Gitsigns next_hunk<CR>'", { expr = true })
-remap("n", "[c", "&diff ? '[c' : '<Cmd>Gitsigns prev_hunk<CR>'", { expr = true })
+end)
+remap("n", "]c", function()
+    require("gitsigns").next_hunk()
+end)
+remap("n", "[c", function()
+    require("gitsigns").prev_hunk()
+end)
 
 -- DAP
 remap("n", "<F5>", function()
     require("dap").continue()
-end, { noremap = true, silent = true })
+end)
 remap("n", "<F6>", function()
     require("dap").toggle_breakpoint()
-end, { noremap = true, silent = true })
+end)
 remap("n", "<F10>", function()
     require("dap").step_over()
-end, { noremap = true, silent = true })
+end)
 remap("n", "<F11>", function()
     require("dap").step_into()
-end, { noremap = true, silent = true })
+end)
 remap("n", "<F12>", function()
     require("dap").step_out()
-end, { noremap = true, silent = true })
+end)
 remap("n", "<Leader>dc", function()
     require("dap").close()
-end, { noremap = true, silent = true })
+end)
 remap("n", "<Leader>dr", function()
     require("dap").repl.open()
-end, { noremap = true, silent = true })
+end)
 remap("n", "<Leader>du", function()
     require("dapui").toggle()
-end, { noremap = true, silent = true })
+end)
 remap("n", "<Leader>de", function()
     require("dapui").eval()
-end, { noremap = true, silent = true })
+end)
 
 -- SnipRun
-remap("n", "<Leader>f", [[<Plug>SnipRun]], { silent = true })
-remap("v", "f", [[<Plug>SnipRun]], { silent = true })
+remap("n", "<Leader>f", "<Plug>SnipRun")
+remap("v", "f", "<Plug>SnipRun")
