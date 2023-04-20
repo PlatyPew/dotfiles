38d319e0b0149f14fbe326c8d3df5357d39bb1c7
Platy <daryl.limkz@gmail.com>
2023-04-20 23:20:35 +0800

    fix(neovim): highlights and stuff
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- configs/neovim/lua/config/lsp.lua
+++ configs/neovim/lua/config/lsp.lua
@@ -1,8 +1,3 @@
--- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
---
---  Add any additional override configuration in the following tables. They will be passed to
---  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
     clangd = {},
     lua_ls = {},
