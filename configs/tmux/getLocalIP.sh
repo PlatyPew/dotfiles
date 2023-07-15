76b154748e7496b77542cdf090448c1dd9820aef
Platy <daryl.limkz@gmail.com>
2023-07-15 14:16:34 +0800

    fix: tmux icons
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- configs/tmux/getLocalIP.sh
+++ configs/tmux/getLocalIP.sh
@@ -4,7 +4,7 @@ NetInterface="$(route -n get 0.0.0.0 2>/dev/null | awk '/interface: / {print $2}
 ip=$(ipconfig getifaddr $NetInterface)
 
 if [ $? -eq 0 ]; then
-	echo -e "\uf817 ${ip}"
+	echo -e "󱊪 ${ip}"
 else
-	echo "\uf818 ${NetInterface} inactive"
+	echo "󰌙 ${NetInterface} inactive"
 fi
